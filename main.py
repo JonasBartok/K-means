92353d6a71ba8ac9a8519171f1730ff5da42deeb
JonasBartok <jonas.bartok@gmail.com>
2024-02-14 15:47:44 +0100

    working version

--- main.py
+++ main.py
@@ -1,146 +1,70 @@
 import random
-
-
-class KMeans:
-    def __init__(self, k: int, data: list):
-        # initializing some vars
-        self.k: int = k
-        self.data: list = data
-        self._data: list = data
-        self.mainlist: list = None
-
-    def random_clusters(self) -> None:
-        for _ in range(self.k):
-            # inserting K number of clusters (None) into random positions
-            self.data.insert(random.randint(0, len(self.data)), None)
-
-
-    def get_cluster_positions(self) -> None:
-        # making a dict with the positions of the clusters for later use
-        self.cluster_positions: list = []
-        # counter for the cluster's position
-        self.clusters_position_counter: int = 0
-        self.cluster_name_counter: int = 0
-
-        for i in self.data:
-            if i == None:
-                # assigning values to dict --example: {1: 0, 2: 6, 3: 10}
-                self.cluster_positions.append(self.clusters_position_counter)
-            self.clusters_position_counter += 1
-
-            self._cluster_positions = self.cluster_positions
-
-
-
-    def calculate_distance(self):
-        if self.mainlist == None:
-            self.mainlist: list = []
-
-
-            for p in range(len(self.data)):
-                self.mainlist.append([])
-
-
-            self.x = None
-            for x in range(len(self.data)):
-                for m in range(len(self.cluster_positions)):
-                    self.mainlist[x].append(abs(self.cluster_positions[m] - x))
-
-            counter = 0
-            for h in range(2):
-                counter = 0
-                for lists in self.mainlist:
-                    if 0 in lists:
-                        self.mainlist.pop(counter)
-                    counter = counter + 1
-        else:
-            self.mainlist: list = []
-            for l in range(len(self.data)):
-                self.mainlist.append([])
-
-            self.a = None
-            for x in range(len(self.data)):
-                for m in range(len(self._cluster_positions)):
-                    self.mainlist[x].append(abs(self.cluster_positions[m] - x))
-
-            counter = 0
-            for h in range(2):
-                counter = 0
-                for lists in self.mainlist:
-                    if 0 in lists:
-                        self.mainlist.pop(counter)
-                    counter = counter + 1
-
-        #print(f"The main list: {self.mainlist}")
-
-
-    def find_smallest_number_in_lists(self):
-        self.smallest_number: float = 0
-        self.smallest_number_index: float = 0
-        self.smallest_number_list: list = []
-
-        global mainlist2
-        self.mainlist2: list = []
-        for p in range(len(self._cluster_positions)):
-            self.mainlist2.append([])
-
-
-        for lists in self.mainlist:
-            self.smallest_number = float(min(lists))
-            self.smallest_number_index = lists.index(self.smallest_number)
-
-            self.mainlist2[self.smallest_number_index].append(self.smallest_number)
-
-        #print(f"mainlist2: {self.mainlist2}")
-
-
-
-    def calculate_mean(self):
-        average: list = []
-
-        for lists in self.mainlist2:
-            try:
-                average.append(float(sum(lists) / len(lists)))
-            except ZeroDivisionError:
-                average.append(0)
-
-        if average == self.cluster_positions:
-            print("The average is equal to the cluster positions")
-            print(f"average: {average}")
-            print(f"average: {sum(average) / len(average)}")
-            print(f"mainlist: {self.mainlist2}")
-            exit()
-
-        self.average: list = average
-
-        self.cluster_positions = average
-
-        #print(f"average: {self.cluster_positions}")
-
-    def exit_with_error(self):
-        print(f"average: {self.average}")
-        print(f"average: {sum(self.average) / len(self.average)}")
-        print(f"mainlist: {self.mainlist2}")
-        exit()
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_blobs
 
 def main():
-    clusters = int(input("Enter the number of clusters: "))
-    data = input("Enter the data: ").split()
-    k = KMeans(k=clusters,
-               data=data)
-    k.random_clusters()
-    k.get_cluster_positions()
-    for _ in range(10000):
-        k.calculate_distance()
-        k.find_smallest_number_in_lists()
-        k.calculate_mean()
-    k.exit_with_error()
+    global data
+    np.random.seed(1)
+
+    #create data
+    blobs = make_blobs(n_samples=50, n_features=2, centers=3)
+    data = blobs[0]
+
+    plt.scatter(data[:,0],data[:,1])
+
+    #creat centroids
+    K = ask_for_number_of_clusters()
+    centroids = make_centroids(K, data)
 
 
-if __name__ == "__main__":
+    while True:
+        ordered_data: list = []
+        for x in data:
+            tmp = []
+            for c in centroids:
+                ordered_data.append([])
+                tmp.append(distance(x, c))
+            minimum = tmp.index(min(tmp))
+            for j in range(len(centroids)):
+                if minimum == j:
+                    ordered_data[j].append(x)
+
+        ordered_data = [np.array(cluster) for cluster in ordered_data if np.array(cluster).any()]
+
+
+
+        mean = [[] for _ in range(K)]
+        for z in range(len(ordered_data)):
+            mean[z] = (ordered_data[z].mean(axis=0))
+
+        mean = np.array(mean).tolist()
+        centroids = np.array(centroids).tolist()
+
+        if mean == centroids:
+            break
+
+        centroids = mean
+
+    for b in ordered_data:
+        plt.scatter(b[:, 0], b[:, 1])
+        plt.scatter(np.array(centroids)[:, 0], np.array(centroids)[:, 1], color='black')
+
+    plt.show()
+
+def make_centroids(k, data):
+    centroids = []
+    for i in range(k):
+        centroids.append(data[random.randint(0,len(data)-1)])
+    return np.array(centroids)
+
+def distance(x,y):
+    return np.linalg.norm(x-y)
+
+def ask_for_number_of_clusters():
+    return int(input("Enter number of clusters: "))
+
+
+
+if __name__ == '__main__':
     main()
-
-
-
-
-
